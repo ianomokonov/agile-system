a16508fb0de7889120c73fb2eb0d88f0f827a317
Ivan Nomokonov <nomokonov.vana@yandex.ru>
2021-03-20 13:18:09 +0300

    AGILE-23

--- src/app/project/project-backlog/project-backlog.component.ts
+++ src/app/project/project-backlog/project-backlog.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskShortView } from 'back/src/models/responses/task-short-view';
+import { ProjectDataService } from 'src/app/services/project-data.service';
 
 @Component({
   selector: 'app-project-backlog',
   templateUrl: './project-backlog.component.html',
-  styleUrls: ['./project-backlog.component.less']
+  styleUrls: ['./project-backlog.component.less'],
 })
 export class ProjectBacklogComponent implements OnInit {
+  public tasks: TaskShortView[];
+  constructor(private projectDataService: ProjectDataService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    this.tasks = this.projectDataService.project.tasks;
   }
-
 }
