dacad5d35aacb2b55e1f4ffb2920980c351ba876
Ivan Nomokonov <nomokonov.vana@yandex.ru>
2021-04-14 22:08:56 +0300

    верстка ретро

--- src/app/project/retro/retro.component.ts
+++ src/app/project/retro/retro.component.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RetroService } from 'src/app/services/retro.service';
@@ -25,5 +26,44 @@ export class RetroComponent implements OnInit {
     this.retroService.read(this.projectId, id).subscribe((retro) => {
       this.retro = retro;
     });
+  }
+
+  public scale(block, event) {
+    if (event.keyCode === 8 || event.keyCode === 46) {
+      return;
+    }
+    const step = 0.1;
+    const minfs = 9;
+    const sch = block.scrollHeight;
+    const h = block.offsetHeight;
+
+    if (sch > h) {
+      const fontsize =
+        parseInt(getComputedStyle(block, null).getPropertyValue('font-size'), 10) - step;
+      if (fontsize >= minfs) {
+        block.style.fontSize = `${fontsize}px`;
+        this.scale(block, event);
+        return;
+      }
+      block.innerText = block.innerText.slice(0, block.innerText.length - 1);
+      this.placeCaretAtEnd(block);
+    }
+  }
+
+  private placeCaretAtEnd(el) {
+    el.focus();
+    if (typeof window.getSelection !== 'undefined' && typeof document.createRange !== 'undefined') {
+      const range = document.createRange();
+      range.selectNodeContents(el);
+      range.collapse(false);
+      const sel = window.getSelection();
+      sel?.removeAllRanges();
+      sel?.addRange(range);
+    } else if (typeof (document.body as any).createTextRange !== 'undefined') {
+      const textRange = (document.body as any).createTextRange();
+      textRange.moveToElementText(el);
+      textRange.collapse(false);
+      textRange.select();
+    }
   }
 }
