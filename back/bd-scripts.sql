580d822b1f928eb87fc09fdb75f497adfd2acded
Nomokonov Ivan <nomokonov.vana@yandex.ru>
2021-04-26 21:48:51 +0300

    AGILE-95

--- back/bd-scripts.sql
+++ back/bd-scripts.sql
@@ -5,9 +5,9 @@ DROP TABLE IF EXISTS `taskHistoryOperations`;
 CREATE TABLE `taskHistoryOperations` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `fieldName` varchar(255) NOT NULL,
-  `newValue` MEDIUMTEXT NOT NULL,
+  `newValue` MEDIUMTEXT NULL,
   `createDate` DATETIME DEFAULT CURRENT_TIMESTAMP,
-  `projectUserId` int(11) NULL,
+  `userId` int(11) NULL,
   `projectTaskId` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) COMMENT 'история изменений задачи';
@@ -15,7 +15,7 @@ CREATE TABLE `taskHistoryOperations` (
 ALTER TABLE
   `taskHistoryOperations`
 ADD
-  FOREIGN KEY (projectUserId) REFERENCES `projectUser` (`id`) ON DELETE SET NULL;
+  FOREIGN KEY (userId) REFERENCES `user` (`id`);
 
 ALTER TABLE
   `taskHistoryOperations`
@@ -31,3 +31,52 @@ ALTER TABLE
   `projectTask`
 ADD
   FOREIGN KEY (lastEditUserId) REFERENCES `user` (`id`);
+
+DELIMITER $$ CREATE TRIGGER update_task
+AFTER
+UPDATE
+  ON projectTask FOR EACH ROW BEGIN
+    IF OLD.name != NEW.name THEN
+        INSERT INTO taskHistoryOperations (fieldName, newValue, userId, projectTaskId)
+            VALUES
+                ('name', NEW.name, NEW.lastEditUserId, NEW.id);
+    END IF;
+    IF OLD.description != NEW.description THEN
+        INSERT INTO taskHistoryOperations (fieldName, newValue, userId, projectTaskId)
+            VALUES
+                ('description', NEW.description, NEW.lastEditUserId, NEW.id);
+    END IF;
+    IF OLD.statusId != NEW.statusId THEN
+        INSERT INTO taskHistoryOperations (fieldName, newValue, userId, projectTaskId)
+            VALUES
+                ('statusId', NEW.statusId, NEW.lastEditUserId, NEW.id);
+    END IF;
+    IF OLD.priorityId != NEW.priorityId THEN
+        INSERT INTO taskHistoryOperations (fieldName, newValue, userId, projectTaskId)
+            VALUES
+                ('priorityId', NEW.priorityId, NEW.lastEditUserId, NEW.id);
+    END IF;
+    IF OLD.typeId != NEW.typeId THEN
+        INSERT INTO taskHistoryOperations (fieldName, newValue, userId, projectTaskId)
+            VALUES
+                ('typeId', NEW.typeId, NEW.lastEditUserId, NEW.id);
+    END IF;
+    IF OLD.projectUserId != NEW.projectUserId 
+    OR OLD.projectUserId IS NULL AND NEW.projectUserId IS NOT NULL
+    OR NEW.projectUserId IS NULL AND OLD.projectUserId IS NOT NULL THEN
+        INSERT INTO taskHistoryOperations (fieldName, newValue, userId, projectTaskId)
+            VALUES
+                ('projectUserId', NEW.projectUserId, NEW.lastEditUserId, NEW.id);
+    END IF;
+    IF OLD.projectSprintId != NEW.projectSprintId THEN
+        INSERT INTO taskHistoryOperations (fieldName, newValue, userId, projectTaskId)
+            VALUES
+                ('projectSprintId', NEW.projectSprintId, NEW.lastEditUserId, NEW.id);
+    END IF;
+    IF OLD.points != NEW.points THEN
+        INSERT INTO taskHistoryOperations (fieldName, newValue, userId, projectTaskId)
+            VALUES
+                ('points', NEW.points, NEW.lastEditUserId, NEW.id);
+    END IF;
+
+END $$ DELIMITER ;
