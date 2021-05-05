a25202c3f2c7901810e8dbb60cfd847b7b03a2af
LASfire <ledashev.andrew@yandex.ru>
2021-05-05 23:23:24 +0300

    Скрипт бд

--- back/AGILE-104.sql
+++ back/AGILE-104.sql
@@ -1,6 +1,12 @@
 ALTER TABLE
   `epics`
 ADD
+  `projectId` int(11) NOT NULL;
+
+
+ALTER TABLE
+  `epics`
+ADD
   FOREIGN KEY (projectId) REFERENCES `project` (`id`) ON DELETE CASCADE;
 
 ALTER TABLE
