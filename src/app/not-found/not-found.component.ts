06a658dde0aca1c58b41f9b5cb0199dc3f4bbe3a
LASfire <ledashev.andrew@yandex.ru>
2021-05-08 17:21:30 +0300

    AGILE-115 Страница 404

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenService } from '../services/token.service';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.less']
+  styleUrls: ['./not-found.component.less'],
 })
 export class NotFoundComponent implements OnInit {
+  constructor(public tokenService: TokenService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
